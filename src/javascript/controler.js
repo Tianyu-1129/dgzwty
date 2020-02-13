8cc1df8451e39dad082d022d8742898601f316be
董广泽 <44460616+suancaiyu1806@users.noreply.github.com>
2020-02-14 02:38:44 +0800

    feat: Complete all tasks basically.

--- src/javascript/controler.js
+++ src/javascript/controler.js
@@ -1,3 +1,4 @@
+var musicon=0
 function bulge(x){
     x.style.width="19.2vw";
     x.style.height="28.5vw"
@@ -6,4 +7,19 @@ function bulge(x){
 function normalImg(x){
     x.style.width="19vw";
     x.style.height="28.3vw"
+}
+function hands(x){
+    x.style.cursor="pointer";
+}
+function startstop(x){
+    if(musicon==1){
+        x.src="../images/icons-nomusic.png";
+        document.getElementById("musicplayer").pause();
+        musicon=0;
+    }else if(musicon==0){
+        x.src="../images/icons-music.png";
+        document.getElementById("musicplayer").play();
+        musicon=1;
+    }
+    console.log(musicon);
 }
