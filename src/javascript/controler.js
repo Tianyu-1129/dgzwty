cf0f6fa188fadb0dd0ca11c0791309df039866c5
董广泽 <44460616+suancaiyu1806@users.noreply.github.com>
2020-02-13 22:23:52 +0800

    feat: Complete the basic framework.
    Complete the basic framework.

---
+++ src/javascript/controler.js
@@ -0,0 +1,9 @@
+function bulge(x){
+    x.style.width="19.2vw";
+    x.style.height="28.5vw"
+    x.style.cursor="pointer";
+}
+function normalImg(x){
+    x.style.width="19vw";
+    x.style.height="28.3vw"
+}
